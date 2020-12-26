f70ec95b2ac7c2e1e1c91fc7da1afe930fb510bd
Daniel Persson <persson.daniel.1990@gmail.com>
2020-12-26 23:16:34 +0100

    Fix service worker link

--- src/registerServiceWorker/registerServiceWorker.js
+++ src/registerServiceWorker/registerServiceWorker.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', async () => {
     try {
-      const { scope } = await navigator.serviceWorker.register('../../public/sw.js')
+      const { scope } = await navigator.serviceWorker.register('../../sw.js')
 
       console.log(`ServiceWorker registration succeeded with scope: ${scope}`)
     } catch (err) {
